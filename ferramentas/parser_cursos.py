7e6fac0f114fc4782783c815266300e164787b36
Bruno Borges Paschoalinoto <bruno@paschoalinoto.com>
2019-01-20 04:06:51 -0200

    FUNCIONA HAHAHAHAHAHAH

--- ferramentas/parser_cursos.py
+++ ferramentas/parser_cursos.py
@@ -24,16 +24,22 @@ if len(sys.argv) < 2:
 
 # armazenar informações parciais de cada curso
 nomearea = None
+nomecarreira = None
 codcarreira = None
 codcurso = None
 
 # armaezanr se a última coisa que vimos foi uma especificação de curso...
 # isso é melhor explicado lá no final do for.
+# mesma coisa pra carreiras agora...
+ultimo_foi_carreira = False
 ultimo_foi_curso = False
 
 # como o nome da carreira fica na linha seguinte, caso isso seja especificado,
 # a próxima linha encontrada vai virar o nome da carreira atual
 waiting_for_carreira = False
+
+# se por qualquer motivo devemos pular de linha...
+pular_proxima = False
 
 # regexes pré-compiladas para achar área, carreira e curso numa linha
 rearea = re.compile(r"Área de (.+)")
@@ -53,55 +59,79 @@ if not len(linhas):
 
 # iterar sobre cada linha
 for linha in linhas:
+    # pular linhas que mandaram pular:
+    if pular_proxima:
+        pular_proxima = False
+        continue
     # remover whitespace desnecessário
     linha = linha.strip()
+    # pular linhas vazias
+    if not len(linha):
+        continue
+    # pular linhas técnicas
+    if "Anexo" in linha:
+        pular_proxima = True
+        continue
     # estamos esperando por um nome de carreira?
     if waiting_for_carreira:
-        resultado[nomearea][codcarreira]["nome_carreira"] = linha
+        resultado[codcarreira]["nome_carreira"] = linha
+        nomecarreira = linha
         # desligar o aviso
         waiting_for_carreira = False
         ultimo_foi_curso = False
+        ultimo_foi_carreira = True
         continue
     area = rearea.match(linha)
     # essa linha especifica a área, colocar ela no JSON e continuar
+    # LER ÁREAS FOI REMOVIDO EU QUERO MORRER
     if area:
         nomearea = area.group(1)
-        resultado[nomearea] = {}
         ultimo_foi_curso = False
+        ultimo_foi_carreira = False
         continue
     carreira = recarreira.match(linha)
     # essa linha especifica uma carreira, adicionar na área atual e esperar pelo
     # nome de carreira na próxima linha
     if carreira:
         codcarreira = carreira.group(1)
-        resultado[nomearea][codcarreira] = {
+        resultado[codcarreira] = {
             "nome_carreira": None,
             "cod_carreira": codcarreira,
+            "area": nomearea,
             "cursos": {}
         }
         waiting_for_carreira = True
         ultimo_foi_curso = False
+        ultimo_foi_carreira = False
         continue
     curso = recurso.match(linha)
     # essa linha especifica um curso, adicionar na carreira atual e prosseguir
     if curso:
         codcurso = curso.group(1)
         nomecurso = curso.group(2)
-        resultado[nomearea][codcarreira]["cursos"][codcurso] = {
+        resultado[codcarreira]["cursos"][codcurso] = {
             "nome_curso": nomecurso,
-            "cod_curso": codcurso
+            "cod_curso": codcurso,
+            "nome_carreira": nomecarreira,
+            "cod_carreira": codcarreira,
+            "area": nomearea
         }
         # avisar que a última coisa que vimos foi uma especificação de curso,
         # aí, caso tenha um curso cujo nome ocupou duas linhas no txt, e a
         # próxima linha tenha texto e não signifique um novo curso, ela será
         # a continuação do nome desse curso
         ultimo_foi_curso = True
+        ultimo_foi_carreira = False
         continue
     # agora, encontramos uma linha que não significa nada em especial
     if ultimo_foi_curso:
         # e ela é continuação do nome do curso atual... eita. só concatenar.
-        resultado[nomearea][codcarreira]["cursos"][codcurso]["nome_curso"] += (
+        resultado[codcarreira]["cursos"][codcurso]["nome_curso"] += (
             " " + linha)
+        continue
+    # mesmo acima, para nomes de carreira
+    if ultimo_foi_carreira:
+        resultado[codcarreira]["nome_carreira"] += " " + linha
         continue
 
 # converter para um JSON bonitinho, printar, e fim
--- publico/scriptoso.js
+++ publico/scriptoso.js
@@ -16,8 +16,28 @@ function make_option(value, text) {
         + "</option>";
 }
 
+// função de utilidade: retorna uma string com n espaços
+function nspaces(n) {
+    var result = "";
+    while (n > 0) {
+        result += " ";
+        n--;
+    }
+    return result;
+}
+
+// função de utilidade: termina uma string com espaços para ela acabar num
+// comprimento mínimo especificado
+function padspaces(str, n) {
+    return str + nspaces(n - str.length);
+}
+
 // elementos da DOM que eu vou usar com frequência
-const criterios = getElementById("criterios");
+const criterios = document.getElementById("criterios");
+const tipo_lista = document.getElementById("origem_lista");
+const textarea = document.getElementById("lista");
+const formato = document.getElementById("formato");
+const cachorro = document.getElementById("cachorro");
 
 // primeiramente, vamos puxar a lista de códigos de curso e carreira.
 var codigos = null;
@@ -28,22 +48,22 @@ if (this.readyState == 4 && this.status == 200) {
         gerar_menu_carreiras();
     }
 };
+ajaj.open("GET", "codigos.json", true);
+ajaj.send();
 
 // segundamente, gerar um menu para lista de carreiras
 // coloquei numa função porque temos que esperar o JSON carregar
 var menu_carreiras = make_option("?", "Especifique uma carreira...");
 function gerar_menu_carreiras() {
-    // iterar sobre cada área...
-    for (var a in codigos) {
-        var area = codigos[a];
-        // ...cada carreira...
-        for (var c in area) {
-            var carreira = area[c];
-            // ...e gerar um item de menu
-            menu_carreiras += make_option(c, c + " - "
-                + carreira["nome_carreira"])
-        }
+    // iterar sobre cada carreira...
+    for (var c in codigos) {
+        var carreira = codigos[c];
+        // ...e gerar um item de menu
+        menu_carreiras += make_option(c, carreira["nome_carreira"] + " (" + c
+            + ")");
     }
+    // sempre começar com um curso
+    add_procurado();
 }
 
 // gera uma lista de cursos para uma dada carreira
@@ -54,19 +74,16 @@ function gerar_menu_cursos(codcarreira) {
     // começar com a opção de "qualquer curso"
     var resultado = make_option("*", "Qualquer curso");
     // achar a carreira em questão
-    for (var a in codigos) {
-        var area = codigos[a];
-        for (var c in area) {
-            if (c != codcarreira) continue;
-            var carreira = area[c];
-            for (var codcurso in carreira["cursos"]) {
-                // ...e gerar o menu
-                var curso = carreira["cursos"][codcurso];
-                resultado += make_option(codcurso, codcurso + " - "
-                    + curso["nome_curso"]);
-            }
-            return resultado;
+    for (var c in codigos) {
+        if (c != codcarreira) continue;
+        var carreira = codigos[c];
+        for (var codcurso in carreira["cursos"]) {
+            // ...e gerar o menu
+            var curso = carreira["cursos"][codcurso];
+            resultado += make_option(codcurso, curso["nome_curso"] + " ("
+                + codcurso + ")");
         }
+        return resultado;
     }
     // a carreira em questão não existe
     return null;
@@ -76,7 +93,7 @@ function gerar_menu_cursos(codcarreira) {
 var procurados = 0;
 
 // adiciona um curso para ser procurado
-function add_curso() {
+function add_procurado() {
     procurados++;
     var id_base = "procurado-" + procurados;
     var seletor = "<div id=\"" + id_base + "\"><select id=\"" + id_base
@@ -86,12 +103,10 @@ function add_curso() {
     criterios.innerHTML += seletor;
 }
 
-// sempre começar com um curso
-add_curso();
-
 // remove um curso para ser procurado
-function remove_curso() {
-    var alvo = getElementById("procurado-" + procurados);
+function remove_procurado() {
+    if (procurados < 2) return;
+    var alvo = document.getElementById("procurado-" + procurados);
     alvo.parentNode.removeChild(alvo);
     procurados--;
 }
@@ -99,19 +114,20 @@ function remove_curso() {
 // chamada quando temos que atualzar a lista de cursos de um certo critério
 function update_procurado(n) {
     var id_base = "procurado-" + n;
-    var car = getElementById(id_base + "-car").value;
-    var cur = getElementById(id_base + "-cur");
-    car.innerHTML = gerar_menu_cursos(car);
-    car.value = "*";
+    var car = document.getElementById(id_base + "-car").value;
+    var cur = document.getElementById(id_base + "-cur");
+    cur.innerHTML = gerar_menu_cursos(car);
+    cur.value = "*";
 }
 
 // gera uma lista de critérios [carreira, curso] que o usuário especificou
-function criterios() {
+function todos_criterios() {
     var lista = [];
     for (var i = 1; i <= procurados; i++) {
         var id_base = "procurado-" + i;
-        var car = getElementById(id_base + "-car").value;
-        var cur = getElementById(id_base + "-cur").value;
+        var car = document.getElementById(id_base + "-car").value;
+        var cur = document.getElementById(id_base + "-cur").value;
+        if (car === "?") continue; // pular carreiras vazias
         lista.push([car, cur]);
     }
     return lista;
@@ -119,15 +135,123 @@ function criterios() {
 
 // testa se um par [carreira, curso] se encaixa num certo critério
 function match_criterio(alvo, criterio) {
-    if (alvo[0] != criterio[0]) return false;
+    if (alvo[1][0] != criterio[0]) return false;
     // carreiras batem
-    return criterio[1] === "*" || criterio[1] === alvo[1];
+    return criterio[1] === "*" || criterio[1] === alvo[1][1];
 }
 
 // testa se um par [carreira, curso] se encaixa em pelo menos um dos critérios
 function match_geral(alvo) {
-    var criterios = criterios();
-    return criterios.reduce(function (passou, criterio) {
+    return todos_criterios().reduce(function (passou, criterio) {
         return passou || match_criterio(alvo, criterio);
     }, false);
+}
+
+// procura relações [nome, [carreira, curso]] em listas de aprovados
+function pesquisa_nomes() {
+    var modalidade = tipo_lista.value;
+    var regex_linha = null;
+    var resultado = [];
+    if (modalidade === "pdf") {
+        regex_linha = /(.+) \d+ (\d+)−(\d+)/g;
+    } else if (modalidade === "txt") {
+        regex_linha = /(.+) +\d+ (\d+)-(\d+)/g;
+    } else {
+        return [];
+    }
+    var linhas = textarea.value.split("\n");
+    for (linha of linhas) {
+        var match = regex_linha.exec(linha);
+        if (!match) continue;
+        var nome = match[1];
+        var car = match[2];
+        var cur = match[3];
+        resultado.push([nome, [car, cur]]);
+    }
+    return resultado;
+}
+
+// gera a lista de nomes pessoas que entram no critério
+function procura_matches() {
+    return pesquisa_nomes().filter(function (alvo) {
+        return match_geral(alvo);
+    });
+}
+
+// itera sobre cada candidato de uma lista
+function iterar(lista, callback) {
+    for (var cand of lista) {
+        var nome = cand[0];
+        var car = cand[1][0];
+        var cur = cand[1][1];
+        var carreira = car + " - " + codigos[car]["nome_carreira"];
+        var curso = cur + " - " + codigos[car]["cursos"][cur]["nome_curso"];
+        callback(nome, car, cur, carreira, curso);
+    }
+}
+
+// converte os candidatos numa planilha HTML
+function planilha(lista) {
+    var html = "<table><tr><td><b>Nome</b></td><td><b>Carreira</b></td><td><b>"
+        + "Curso</b></td></tr>";
+    iterar(lista, function(nome, car, cur, carreira, curso) {
+        html += "<tr><td>" + nome + "</td><td>" + carreira + "</td><td>" + curso
+            + "</td></tr>";
+    });
+    html += "</table>";
+    return html;
+}
+
+// converte os candidatos numa lista lindinha em texto puro
+function txt(lista) {
+    var maxcar = 0;
+    var maxnome = lista.reduce(function (max, cand) {
+        maxcar = Math.max(maxcar, codigos[cand[1][0]]["nome_carreira"].length);
+        return Math.max(max, cand[0].length);
+    }, 0);
+    //lista = [["NOME", ["CARREIRA", "CURSO\n"]]] + lista;
+    var resultado = "";
+    var basepad = nspaces(6);
+    iterar(lista, function (nome, car, cur, carreira, curso) {
+        resultado += padspaces(nome, maxnome) + basepad
+            + padspaces(carreira, maxcar) + basepad + curso + "\n";
+    });
+    return resultado;
+}
+
+// cria um link virtual para os dados
+
+
+// hora do show: gerar os dados pedidos e mostrar pro usuário!
+function hora_do_show() {
+    var quero = formato.value;
+    var matches = procura_matches();
+    if (!matches.length) {
+        cachorro.innerHTML = "";
+    }
+    if (quero === "lista") {
+        // gerar lista em texto puro numa nova guia
+        var dados = encodeURIComponent(txt(matches));
+        var href = "data:text/plain," + dados;
+        var win = window.open();
+        win.document.write("<iframe src=\"" + href  + "\" frameborder=\"0\" "
+            + "style=\"border:0; top:0px; left:0px; bottom:0px; right:0px; "
+            + "width:100%; height:100%;\" allowfullscreen></iframe>");
+    } else if (quero === "html") {
+        // planilha HTML numa nova guia
+        var dados = encodeURIComponent(planilha(matches));
+        var href = "data:text/html," + dados;
+        var win = window.open();
+        win.document.write("<iframe src=\"" + href  + "\" frameborder=\"0\" "
+            + "style=\"border:0; top:0px; left:0px; bottom:0px; right:0px; "
+            + "width:100%; height:100%;\" allowfullscreen></iframe>");
+    } else if (quero == "xls") {
+        // xls para download
+        var dados = btoa(planilha(matches));
+        var lel = document.createElement("a");
+        var data_type = "data:application/vnd.ms-excel";
+        lel.href = data_type + ";base64," + dados;
+        lel.download = "fuvest_aprovados.xls";
+        lel.click();
+    }
 }
